8f1ceafcb64c8906b44057fa3d93c126f3a791a1
Pac12Run8000 <norbert298@gmail.com>
2015-12-14 17:22:23 -0800

    version 0.02

--- StreetSpartanWorkout/ViewController.swift
+++ StreetSpartanWorkout/ViewController.swift
@@ -10,9 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lblTimeDisplay: UILabel!
+    @IBOutlet weak var btnStartStop: UIButton!
+    @IBOutlet weak var btnReset: UIButton!
+    @IBOutlet weak var tblExercise: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.btnStartStop.layer.borderWidth = 3
+        self.btnStartStop.layer.borderColor = UIColor.blackColor().CGColor
+        self.btnStartStop.layer.cornerRadius = self.btnStartStop.frame.size.width / 2
+       
+        self.btnReset.layer.borderWidth = 3
+        self.btnReset.layer.borderColor = UIColor.blackColor().CGColor
+        self.btnReset.layer.cornerRadius = self.btnReset.frame.size.width / 2
     }
 
     override func didReceiveMemoryWarning() {
