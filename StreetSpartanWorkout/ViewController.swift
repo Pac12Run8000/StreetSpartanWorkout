96d9333617d3e699789caade86c22572b02d788d
Pac12Run8000 <norbert298@gmail.com>
2015-12-18 20:03:34 -0800

    version 0.04

--- StreetSpartanWorkout/ViewController.swift
+++ StreetSpartanWorkout/ViewController.swift
@@ -15,7 +15,9 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     var fractions:Int = 0
     var startStopWatch:Bool = true
     var stopWatchString:String = ""
-    //let arrayWorkout[String, String] = ["", ""]
+    
+    
+    let arrayWorkout:[String] = ["4 Sets x 8 Chin Ups", "4 Sets x 8 Pull Ups", "4 Sets x 6 Barbel Front Squat", "4 Sets x 6 Dive Bombers", "2 Sets x 18 Calf Raises", "4 Sets x 6 Dips", "4 Sets x 8 Bench Press", "3 Sets x 15 Push Ups", "3 Sets x 15 Diamond Push Ups", "3 Sets x 15 Military Press", "3 Sets x 15 Chin Up (Negatives)", "4 Sets x 8 Pull Up (Negatives)"]
     
     
     @IBOutlet weak var lblTimeDisplay: UILabel!
@@ -44,6 +46,27 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         if (seconds == 60) {
             minutes += 1
             seconds = 0
+        }
+        
+        if (seconds == 30) {
+            seconds = 0
+            minutes = 0
+            fractions = 0
+            stopWatchString = "00:00:00"
+            lblTimeDisplay.text = stopWatchString
+            timer.invalidate()
+            
+            let myAlert = UIAlertController(title: "Workout Alert", message: "30 seconds have passed. Times Up!", preferredStyle: UIAlertControllerStyle.Alert)
+            let myAction = UIAlertAction(title: "Start your next set", style: UIAlertActionStyle.Cancel, handler: { (ACTION) -> Void in
+                
+                if (self.startStopWatch == false) {
+                    self.btnStartStop.setTitle("Start", forState: UIControlState.Normal)
+                    self.startStopWatch = true
+                }
+            })
+            myAlert.addAction(myAction)
+            self.presentViewController(myAlert, animated: true, completion: nil)
+            
         }
         let fractionString = fractions > 9 ? "\(fractions)" : "0\(fractions)"
         let secondString = seconds > 9 ? "\(seconds)" : "0\(seconds)"
@@ -78,12 +101,12 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = UITableViewCell(style: UITableViewCellStyle.Subtitle, reuseIdentifier: "Cell")
         //cell.backgroundColor = self.view.backgroundColor
-        cell.textLabel!.text = "3 sets of 10 reps [30 second rest]"
-        cell.detailTextLabel?.text = "Diamond Push Up"
+        cell.textLabel!.text = arrayWorkout[indexPath.row]
+        cell.detailTextLabel?.text = "[30 second rest]"
         return cell
     }
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return arrayWorkout.count
     }
 
 
